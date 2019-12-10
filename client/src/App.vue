4a04e4b8018e275cade3097123d274eeb226a37b
AlexMenor <alexmenor@github.com>
2019-12-10 18:17:05 +0100

    home component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-img max-width="50px" max-height="50px" src="./assets/cat.svg"></v-img>
+      <v-img
+        max-width="50px"
+        max-height="50px"
+        src="./assets/cat.svg"
+        class="clickable"
+        @click="$router.push('/')"
+      ></v-img>
       <v-toolbar-title
         @click="$router.push('/')"
         class="display-1 font-weight-light text-uppercase clickable"
-        >mundopet</v-toolbar-title
-      >
+      >mundopet</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn color="primary" to="/login">Login/Registro</v-btn>
     </v-app-bar>
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,3 +1,109 @@
 <template>
-  <h1>This is home</h1>
+  <div class="centered">
+    <div class="home-header">
+      <div>
+        <h1>¡QUÉ MONOS!</h1>
+        <h2>Descubre a tu nueva mascota</h2>
+      </div>
+      <div>
+        <h2>Selecciona tu provincia</h2>
+        <v-select dark solo :items="provincias" v-model="provinciaSeleccionada"></v-select>
+      </div>
+    </div>
+    <v-card width="60%" min-height="60%">
+      <v-container>
+        <v-row>
+          <v-col v-for="a in animals" :key="a.id">
+            <v-card outlined>
+              <v-img width="250px" height="250px" :src="a.picture"></v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    animals: [],
+    provincias: [
+      "Alava",
+      "Albacete",
+      "Alicante",
+      "Almería",
+      "Asturias",
+      "Avila",
+      "Badajoz",
+      "Barcelona",
+      "Burgos",
+      "Cáceres",
+      "Cádiz",
+      "Cantabria",
+      "Castellón",
+      "Ciudad Real",
+      "Córdoba",
+      "La Coruña",
+      "Cuenca",
+      "Gerona",
+      "Granada",
+      "Guadalajara",
+      "Guipúzcoa",
+      "Huelva",
+      "Huesca",
+      "Islas Baleares",
+      "Jaén",
+      "León",
+      "Lérida",
+      "Lugo",
+      "Madrid",
+      "Málaga",
+      "Murcia",
+      "Navarra",
+      "Orense",
+      "Palencia",
+      "Las Palmas",
+      "Pontevedra",
+      "La Rioja",
+      "Salamanca",
+      "Segovia",
+      "Sevilla",
+      "Soria",
+      "Tarragona",
+      "Santa Cruz de Tenerife",
+      "Teruel",
+      "Toledo",
+      "Valencia",
+      "Valladolid",
+      "Vizcaya",
+      "Zamora",
+      "Zaragoza"
+    ],
+    provinciaSeleccionada: "Burgos"
+  }),
+  mounted() {
+    for (let i = 0; i < 20; i++) {
+      this.animals.push({
+        picture: "https://source.unsplash.com/random",
+        id: i
+      });
+    }
+  }
+};
+</script>
+
+
+<style>
+.home-header {
+  text-align: start;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-around;
+  width: 100%;
+  padding: 15em;
+  background-image: url("https://images.unsplash.com/photo-1496522650602-5988dce09660?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80");
+  color: white;
+}
+</style>
