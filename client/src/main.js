5741c65e1b6b54ab715d101c9c803b37b1fa6bda
Torchu <victor.cabritag@gmail.com>
2019-12-13 18:48:43 +0100

    FlipCard bug fixed

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false;
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
     ? "https://protectoras.herokuapp.com"
-    : "https://protectoras.herokuapp.com";
+    : "http://localhost:5000";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 import router from "./router";
